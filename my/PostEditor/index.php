deabd6fe8cdf892c6420441d90b2b8a766d7d65d
Jumper Luko <jumper.luko@gmail.com>
2019-02-05 23:08:07 -0200

    Work very fine

--- my/PostEditor/index.php
+++ my/PostEditor/index.php
@@ -45,7 +45,7 @@
 					$consulta = "select post.id_post,post.name_post from post ORDER BY post.name_post;";
 					$query = mysqli_query($conexao,$consulta);
 					$query2 = $query;
-					echo '<form method="get" action=".#post"> <select name="query">';
+					echo '<form method="get" action=".?post=edit#post"> <select name="query">';
 					while($line  =  mysqli_fetch_array($query)){
 						echo '<option value="'.$line['id_post'].'">'.$line['name_post'].'</option>';
 					   	// echo '<div style="width:100%;">'.$line['post_post'].'</div>';
@@ -64,7 +64,7 @@
 			   		                	<input type="submit" value="Atualizar" class="postTitle"/>
 			   						</div>
 			   		            </form>
-								<iframe src="tryEditor/tryEditor.html" style="resize: both;overflow: auto;width:100%;height:500px;margin:300px 0 0 0;resize: vertical;" noresize="resize"></iframe>
+								<iframe src="tryEditor/tryEditor.html" style="resize: vertical;overflow: auto;width:100%;height:500px;margin:300px 0 0 0;" noresize="resize"></iframe>
 								<script src="ckeditor4.9.2-full-personalized/ckeditor.js"></script>
 								<div style="margin:40px 0 0 0;">
 									<textarea name="post_area" id="post_area" rows="10" cols="80" style="color:#000000;font-family:Arial,Calibri;resize: vertical;">'.$line['post_post'].'</textarea>
@@ -73,34 +73,37 @@
 								';
 						}
 					}
-					$consulta = "select post.id_post,post.name_post from post ORDER BY post.name_post;";
-					$query = mysqli_query($conexao,$consulta);
-					echo '<form method="post" action="post_proc.php" style="margin:300px 0 0 0;"> <select name="idpost">';
-					while($line  =  mysqli_fetch_array($query)){
-						echo '<option value="'.$line['id_post'].'">'.$line['name_post'].'</option>';
-					}
-					echo '</select><input type="hidden" name="post" value="delete"><input type="submit" value="Apagar"/></form>';
+					echo '<a href=".?post=delet#post"><button style="margin:300px 0 0 0;">Deletar Posts?</button></a>';
 				}elseif($_GET['post']=='insert'){
 					echo '
 					<form method="post" action="post_proc.php" class="postEdit">
 		                <input name="title" type="text" placeholder="Titulo" class="col-12 postTitle"/>
 		                <input name="imgCapa" type="text" placeholder="Link da imagem de capa" class="col-12 postTitle"/>
-		                <textarea name="post_area" rows="10" style="color:#000000;font-family:Arial,Calibri;width:100%;background-color: #ffffff;"></textarea>
+		                <textarea name="post_area" rows="10" style="color:#000000;font-family:Arial,Calibri;width:100%;background-color: #ffffff;resize: vertical;"></textarea>
 						<input type="hidden" name="post" value="insert">
 						<div class="col-12 center">
 		                	<input type="submit" value="Inserir" class="postTitle"/>
 						</div>
 		            </form>
-					<iframe src="tryEditor/tryEditor.html" style="resize: both;overflow: auto;width:100%;height:500px;margin:300px 0 0 0" noresize="resize"></iframe>
+					<iframe src="tryEditor/tryEditor.html" style="resize: vertical;overflow: auto;width:100%;height:500px;margin:300px 0 0 0" noresize="resize"></iframe>
 					<script src="ckeditor4.9.2-full-personalized/ckeditor.js"></script>
 					<div style="margin:40px 0 50vh 0;">
-						<textarea name="post_area" id="post_area" rows="10" cols="80" style="color:#000000;font-family:Arial,Calibri;">
+						<textarea name="post_area" id="post_area" rows="10" cols="80" style="color:#000000;font-family:Arial,Calibri;resize: vertical;">
 							This is my textarea to be replaced with CKEditor.
 						</textarea>
 						<script>CKEDITOR.replace( post_area );</script>
 					</div>
 					';
 
+				}elseif($_GET['post']=='delet'){
+					require('connect.php');
+					$consulta = "select post.id_post,post.name_post from post ORDER BY post.name_post;";
+					$query = mysqli_query($conexao,$consulta);
+					echo '<form method="post" action="post_proc.php"> <select name="idpost">';
+					while($line  =  mysqli_fetch_array($query)){
+						echo '<option value="'.$line['id_post'].'">'.$line['name_post'].'</option>';
+					}
+					echo '</select><input type="hidden" name="post" value="delete"><input type="submit" value="Apagar"/></form>';
 				}
 			}
 			unset($conexao);
